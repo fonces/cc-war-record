2e647f4153d51073956335049eb4a1060adfc76f
fonces <fonces.xyz@gmail.com>
2025-10-13 18:01:08 +0900

    Updates

--- assets/EmptyState-BkSCG_-G.js
+++ assets/EmptyState-BkSCG_-G.js
@@ -1,4 +1,4 @@
-import{C as e,E as t,L as n,M as r,j as i}from"./index-Br5uo2xh.js";var a=n(i()),o=r.div`
+import{C as e,E as t,M as n,N as r,R as i}from"./index-DgPsM0Ei.js";var a=i(n()),o=r.div`
   display: flex;
   flex-direction: column;
   align-items: center;
