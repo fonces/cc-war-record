6d564684c1c870ae50970ec57b873995c65d0ad1
fonces <fonces.xyz@gmail.com>
2025-10-24 19:27:54 +0900

    [refactor] NewSeasonPageをfeaturesディレクトリに移動と未使用localeを削除
    - NewSeasonPageをhistoriesからnewフィーチャーに移動
    - 未使用のlocaleキーを削除(createFirstSeason, notFound)

--- src/routes/new.tsx
+++ src/routes/new.tsx
@@ -5,5 +5,5 @@ import { createFileRoute, lazyRouteComponent } from "@tanstack/react-router";
  * 新しいシーズンを作成するフォーム画面
  */
 export const Route = createFileRoute("/new")({
-  component: lazyRouteComponent(() => import("@/features/histories").then((m) => ({ default: m.NewSeasonPage }))),
+  component: lazyRouteComponent(() => import("@/features/new").then((m) => ({ default: m.NewSeasonPage }))),
 });
